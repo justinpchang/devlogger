92d4ae578bcd2a1842548f81f3b1bc91e9de3162
Justin Chang <justin.p.chang@gmail.com>
2023-06-29 11:02:10 -0400

    Replace indigo with mulberry on profile edit page

--- web/src/pages/profile/edit.tsx
+++ web/src/pages/profile/edit.tsx
@@ -118,7 +118,7 @@ export default function EditProfile() {
                     Username
                   </label>
                   <div className="mt-2">
-                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-mulberry-600 sm:max-w-md">
                       <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">
                         in.public/
                       </span>
@@ -148,7 +148,7 @@ export default function EditProfile() {
                       rows={3}
                       value={about}
                       onChange={(e) => setAbout(e.target.value)}
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-mulberry-600 sm:text-sm sm:leading-6"
                     />
                   </div>
                   <p className="mt-3 text-sm leading-6 text-gray-600">
@@ -166,7 +166,7 @@ export default function EditProfile() {
             <button
               type="submit"
               disabled={isUpdatingUser || !isValid}
-              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:opacity-50 disabled:cursor-not-allowed"
+              className="rounded-md bg-mulberry-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-mulberry-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-mulberry-600 disabled:opacity-50 disabled:cursor-not-allowed"
             >
               Save
             </button>
