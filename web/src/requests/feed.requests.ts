3d4579125cc1f33405e5f9bf0e1ed2bfbbcb06b3
Justin Chang <justin.p.chang@gmail.com>
2023-07-14 09:42:50 -0400

    Add auto-discovery links to all relevant pages

---
+++ web/src/requests/feed.requests.ts
@@ -0,0 +1,14 @@
+import { apiService } from "@/services/ApiService";
+
+export const getLink = (
+  type: "rss" | "atom",
+  data: { slug: string } | { username: string } | null
+) => {
+  if (!data) return `${apiService.instance.defaults.baseURL}/updates.${type}`;
+
+  if ("slug" in data)
+    return `${apiService.instance.defaults.baseURL}/projects/${data.slug}/updates.${type}`;
+
+  if ("username" in data)
+    return `${apiService.instance.defaults.baseURL}/users/${data.username}/updates.${type}`;
+};
