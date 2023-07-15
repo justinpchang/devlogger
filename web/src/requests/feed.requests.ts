5e59d60fd7d976b57ce43e5dd27c45ed07e86777
Justin Chang <justin.p.chang@gmail.com>
2023-07-15 17:15:22 -0400

    Trigger deploy

--- web/src/requests/feed.requests.ts
+++ web/src/requests/feed.requests.ts
@@ -5,10 +5,8 @@ export const getLink = (
   data: { slug: string } | { username: string } | null
 ) => {
   if (!data) return `${apiService.instance.defaults.baseURL}/updates.${type}`;
-
   if ("slug" in data)
     return `${apiService.instance.defaults.baseURL}/projects/${data.slug}/updates.${type}`;
-
   if ("username" in data)
     return `${apiService.instance.defaults.baseURL}/users/${data.username}/updates.${type}`;
 };
